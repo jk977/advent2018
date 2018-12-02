04223264fa3b57d0464353b899bf32dc0cb975a5
jk977 <jdk64@comcast.net>
2018-12-02 10:56:41 -0800

    Implementing part 1, failed part 2

--- day1/app/Main.hs
+++ day1/app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import Data.List
 import Lib
 
+getChanges :: IO [Int -> Int]
+getChanges = getContents >>= return . map parseChange . init . splitOn "\n"
+
+part1 :: IO ()
+part1 = getChanges >>= print . last . getFreqs 0
+
+part2 :: IO ()
+part2 = getChanges >>= print . firstDup . getFreqs 0 . cycle
+
 main :: IO ()
-main = undefined
+main = part2
--- day1/src/Lib.hs
+++ day1/src/Lib.hs
@@ -3,15 +3,34 @@ module Lib where
 parseOp :: Char -> Int -> Int -> Int
 parseOp c = case c of
     '+' -> (+)
-    '-' -> subtract
+    '-' -> (-)
 
 parseChange :: String -> Int -> Int
 parseChange (op:num) = parseOp op $ read num
 
 splitOn :: Eq a => [a] -> [a] -> [[a]]
-splitOn delim s = map reverse $ reverse (splitOn' [[]] delim s) where
+splitOn delim s = map reverse . reverse $ splitOn' [[]] delim s where
     splitOn' cur@(top:ts) (d:ds) (x:xs)
         | d == x = splitOn' cur ds xs
         | otherwise = splitOn' ((x:top) : ts) delim xs
-    splitOn' current [] xs = splitOn' ([]:current) delim xs
-    splitOn' current _ [] = current
+    splitOn' cur [] xs = splitOn' ([] : cur) delim xs
+    splitOn' cur _ [] = cur
+
+getFreqs :: Int -> [Int -> Int] -> [Int]
+getFreqs cur [] = return cur
+getFreqs cur (f:fs) = cur : getFreqs (f cur) fs
+
+update :: (Eq a, Num b) => a -> (b -> b) -> [(a,b)] -> [(a,b)]
+update x f ls
+    | x `elem` keys = incKey <$> ls
+    | otherwise = (x, f 0) : ls
+    where
+        keys = map fst ls
+        incKey (a,b) = (a, if a == x then f b else b)
+
+firstDup :: Eq a => [a] -> Maybe a
+firstDup ls = firstDup' [] ls where
+    firstDup' _ [] = Nothing
+    firstDup' counts (x:xs)
+        | lookup x counts > pure 0 = Just x
+        | otherwise = firstDup' (update x succ counts) xs
