9782a1896de0e83244c23eb52ca650263e659a25
jk977 <jdk64@comcast.net>
2018-12-04 23:49:02 -0800

    Migrating to date/time library in D4P1

--- src/days/Day4.hs
+++ src/days/Day4.hs
@@ -3,29 +3,18 @@ module Day4 where
 import Control.Monad (forM)
 import Control.Monad.ST
 
+import Data.Time.Clock
+import Data.Time.Format
+
 import Data.STRef
 import Data.Either (rights)
 import Data.List (sortOn)
 
 import Text.Parsec
 
-import Util (readInt, nInt)
+import Util
 
-data Date = Date {
-    year :: Int,
-    month :: Int,
-    day :: Int
-} deriving (Show, Eq, Ord)
-
-data Time = Time {
-    hour :: Int,
-    minute :: Int
-} deriving (Show, Eq, Ord)
-
-data DateTime = DateTime {
-    date :: Date,
-    time :: Time
-} deriving (Show, Eq, Ord)
+type DateTime = UTCTime
 
 data Action = Sleep | Wake | StartShift deriving (Show, Eq)
 
@@ -41,44 +30,11 @@ isImplicit :: Guard -> Bool
 isImplicit ImplicitId = True
 isImplicit _          = False
 
-minsInHr    = 60
-minsInDay   = 24 * minsInHr
-minsInYr    = 365 * minsInDay
-
-daysInMo :: Int -> Int
-daysInMo n
-    | n == 2              = 28
-    | n `elem` [4,6,9,11] = 30
-    | otherwise           = 31
-
-minsInDate :: Date -> Int
-minsInDate d = yr + mo + dy where
-    yr = minsInYr * year d
-    mo = sum $ (minsInDay*) . daysInMo <$> [1..month d]
-    dy = minsInDay * day d
-
-minsInTime :: Time -> Int
-minsInTime t = minsInHr * (hour t) + (minute t)
-
-minsInDT :: DateTime -> Int
-minsInDT (DateTime d t) = minsInDate d + minsInTime t
+parseDT :: String -> DateTime
+parseDT = parseTimeOrError False defaultTimeLocale "%Y-%-m-%-d %H:%M"
 
 timestamp :: Monad m => ParsecT String u m DateTime
-timestamp = between (char '[') (char ']') $ do
-    yr <- nInt 4
-    char '-'
-    mo <- nInt 2
-    char '-'
-    dy <- nInt 2
-    space
-    hr <- nInt 2
-    char ':'
-    mn <- nInt 2
-
-    let d = Date yr mo dy
-        t = Time hr mn
-
-    return $ DateTime d t
+timestamp = between (char '[') (char ']') $ parseDT <$> many (noneOf "]")
 
 sleep :: Monad m => ParsecT String u m (Guard, Action)
 sleep = string "falls asleep" >> return (ImplicitId, Sleep)
@@ -101,7 +57,6 @@ logLine = do
     (n, action) <- guardAction
     return $ Log n action dt
 
-readLogs :: IO [Log]
 readLogs = getContents
     >>= return
         . rights
@@ -126,7 +81,7 @@ convertImplicits logs = runST $ do
 part1 :: IO ()
 part1 = do
     logs <- convertImplicits . sortOn dateTime <$> readLogs
-    putStrLn . unlines . map show $ logs
+    print logs
 
 part2 :: IO ()
 part2 = undefined
