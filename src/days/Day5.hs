4b48197c39d1431c415a2001dd5e8705e0344f28
jk977 <jdk64@comcast.net>
2018-12-06 20:15:32 -0800

    Refactoring helper function in D5P1

--- src/days/Day5.hs
+++ src/days/Day5.hs
@@ -5,14 +5,15 @@ import Data.Char
 (~=~) :: Char -> Char -> Bool
 a ~=~ b = (toLower a) == (toLower b)
 
-react' :: String -> String -> String
-react' prev (x:y:ys)
-    | not (x ~=~ y) || (x == y) = react' (x:prev) (y:ys)
-    | otherwise = react' [] (reverse prev ++ ys)
-react' prev s = prev ++ s
-
 react :: String -> String
-react = react' []
+react = react' [] where
+    -- prev tracks the previous items so when a change happens, the reaction
+    -- can start from the beginning again. prev is in reverse to prevent
+    -- list concatenation every time no reaction occurs
+    react' prev (x:y:ys)
+        | not (x ~=~ y) || (x == y) = react' (x:prev) (y:ys) -- no reaction
+        | otherwise                 = react' [] (reverse prev ++ ys)
+    react' prev s = prev ++ s -- final result
 
 part1 :: IO ()
 part1 = getLine >>= print . length . react
