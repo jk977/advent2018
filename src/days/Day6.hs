f10051b310a9a542f28fe0c8164d59ecb75b588b
jk977 <jdk64@comcast.net>
2018-12-09 20:04:32 -0800

    Last commit on D6P1 before moving on (for now)
    Will either attempt to finish the naive solution or implement Fortune's
    algorithm for Voronoi diagrams

--- src/days/Day6.hs
+++ src/days/Day6.hs
@@ -1,9 +1,13 @@
 module Day6 where
 
+import Control.Monad (forM_)
+
 import Data.Function (on)
-import Data.List     (sortOn)
-import Debug.Trace   (trace)
-import Util          (Point(..), subPt)
+import Data.List     (sortOn, nub)
+import Data.Maybe    (catMaybes)
+
+import GHC.Exts (groupWith)
+import Util     (Point(..), subPt, enclose, pointsIn)
 
 data FlatRayType = Horiz | Vert
     deriving (Show, Eq)
@@ -17,6 +21,32 @@ data FlatRay = FlatRay {
     direction :: Direction,
     start     :: Point
 } deriving (Show, Eq)
+
+dirComponents :: FlatRay -> (Int,Int)
+dirComponents (FlatRay Horiz d p)
+    | d == Positive = (1,0)
+    | otherwise     = (-1,0)
+dirComponents (FlatRay Vert d p)
+    | d == Positive = (0,1)
+    | otherwise     = (0,-1)
+
+intersection :: FlatRay -> FlatRay -> Maybe Point
+intersection r1@(FlatRay rt1 d1 p1) r2@(FlatRay rt2 d2 p2) = do
+    let (dx1,dy1) = dirComponents r1
+        (dx2,dy2) = dirComponents r2
+        diffX = x p2 - x p1
+        diffY = y p2 - y p1
+        det = dx2 * dy1 - dy2 * dx1
+
+        d1 = (diffY * dx2 - diffX * dy2) `div` det
+        d2 = (diffY * dx1 - diffX * dy1) `div` det
+
+    if (det /= 0) && (d1 >= 0) && (d2 >= 0) then
+        let x' = x p1 + d1 * dx1
+            y' = y p1 + d1 * dy1
+        in  return $ Point x' y'
+    else
+        Nothing
 
 -- gets parallel distance from point to start of ray
 flatDistanceFrom :: FlatRay -> Point -> Int
@@ -41,7 +71,6 @@ distanceFrom ray@(FlatRay rt d p') p
                (d == Positive) && (flatDist >= 0)
             || (d == Negative) && (flatDist <= 0)
 
--- checks if ray is limited by point.
 -- this is used four times (once for each cardinal direction) for each source
 -- point to detect if the area occupied by the source point is finite
 isLimitedBy :: FlatRay -> Point -> Bool
@@ -49,23 +78,52 @@ isLimitedBy ray p = case distanceFrom ray p of
     Just dist   -> dist <= abs (flatDistanceFrom ray p)
     _           -> False
 
-hasFiniteAreaIn :: [Point] -> Point -> Bool
-hasFiniteAreaIn sources p = and $ do
+raysFrom :: Point -> [FlatRay]
+raysFrom p = do
     rt  <- [Horiz, Vert]
     dir <- [Positive, Negative]
+    return $ FlatRay rt dir p
 
-    let ray      = FlatRay rt dir p
-        others   = filter (/=p) sources
-        isFinite = or $ (ray `isLimitedBy`) <$> others
+hasFiniteAreaIn :: [Point] -> Point -> Bool
+hasFiniteAreaIn sources p = and $ do
+    let others  = filter (/=p) sources
+    ray         <- raysFrom p
+    return . or $ (ray `isLimitedBy`) <$> others
 
-    return isFinite
+-- gets rough bounds of area taken by point to reduce number of calculations
+boundsOf :: Point -> [Point] -> [Point]
+boundsOf p sources = nub . catMaybes $ do
+    source <- filter (/=p) sources
+    r1     <- raysFrom p
 
-areaTakenBy :: Point -> [Point] -> Int
-areaTakenBy p ps = undefined
+    if r1 `isLimitedBy` source then do
+        r2 <- raysFrom source
+        return $ intersection r1 r2
+    else
+        return Nothing
 
 manhattanDistance :: Point -> Point -> Int
 manhattanDistance p1 p2 = abs x + abs y where
     Point x y = subPt p1 p2
+
+closestTo :: [Point] -> [Point] -> [(Point, Maybe Point)]
+closestTo ps sources = do
+    p <- ps
+
+    let others = filter (/=p) sources
+        labeled = zip sources $ manhattanDistance p <$> sources
+        options = map fst . head $ groupWith snd labeled
+
+    if length options /= 1 then
+        return (p, Nothing)
+    else
+        return (p, Just $ head options)
+
+areaTakenBy :: Point -> [Point] -> Int
+areaTakenBy p sources = length . filter (maybe False (==p) . snd) $ sourceMap where
+    bounds = boundsOf p sources
+    potentials = pointsIn $ enclose bounds
+    sourceMap = closestTo potentials sources
 
 readPoint :: String -> Point
 readPoint s = Point x y where
@@ -77,7 +135,9 @@ part1 = do
     seeds <- map readPoint . lines <$> getContents
 
     let finites = filter (hasFiniteAreaIn seeds) seeds
-    putStrLn . unlines . map show $ finites
+        areas  = length . flip boundsOf seeds <$> finites
+
+    undefined
 
 part2 :: IO ()
 part2 = undefined
